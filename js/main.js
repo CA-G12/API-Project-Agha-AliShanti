a0ca30275841629c7fae75f9b85446d74dea06e6
Mohammed alagha <modyagha7@gmail.com>
2022-08-03 09:33:12 +0300

    create the function wich handle the case that the user have acount in gitHup but dont have repos related #20

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function fetch(url, cb, container) {
     if (xhr.readyState == 4) {
       if (xhr.status == 200) {
         cb(JSON.parse(xhr.responseText), container);
+        
       } else {
         console.log("Error " + xhr.status);
       }
@@ -25,32 +26,45 @@ function generateUserDom(user, container) {
   <div class="userName">${user.name || user.login}</div>
   
   <div class="userInfo">
-  <div class="userBio">${user.bio}</div>
+  <div class="userBio">${user.bio ||'No Bio'}</div>
   <div class="userfol">
             <div class="foling">Followers: ${user.following}</div>
             <div class="fols">Following: ${user.followers}</div>
   </div>
-  <div class="userEmail">${user.email}</div>
-  <div class="userLoc">${user.location}</div></div>
+  <div class="userEmail">${user.email || 'No Email'}</div>
+  <div class="userLoc">${user.location || 'No location'}</div></div>
   
       `;
 }
 function generateReposDom(repos, container) {
-  container.innerHTML = ``;
-  repos.forEach((repo) => {
-    container.innerHTML += `
-      <div class="repo">
-          <div class="repoName"> ${repo.name}</div>
-          <div class="repoStars"><i class="fa-solid fa-star"></i>  ${
-            repo.stargazers_count
-          }</div>
-          <div class="repoLangs"><i class="fa-solid fa-code"></i> ${
-            repo.language
-          }</div>
-          <div class="repoDate"> <i class="fa-solid fa-calendar-days"> </i> ${new Date(
-            repo.updated_at
-          ).toLocaleDateString()}</div>
-      </div>
-      `;
-  });
+  if (repos.length > 0) {
+    container.innerHTML = ``;
+    repos.forEach((repo) => {
+      container.innerHTML += `
+        <div class="repo">
+            <div class="repoName"> ${repo.name}</div>
+            <div class="repoStars"><i class="fa-solid fa-star"></i>  ${
+              repo.stargazers_count
+            }</div>
+            <div class="repoLangs"><i class="fa-solid fa-code"></i> ${
+              repo.language
+            }</div>
+            <div class="repoDate"> <i class="fa-solid fa-calendar-days"> </i> ${new Date(
+              repo.updated_at
+            ).toLocaleDateString()}</div>
+        </div>
+        `;
+    });
+  } else {
+    fetch(
+      "https://api.giphy.com/v1/gifs/search?q=kitchen-looney-tunes-empty&api_key=TLbbBcJE8n7CLqs4EvB5Uo342SwEcFYI",
+      generateEmptyRepoDom,
+      container
+    );
+  }
+  window.scrollTo(0, 500)
+}
+function generateEmptyRepoDom(data, container) {
+  let url = data.data[0].images.downsized_medium.url;
+  container.innerHTML = `<img src='${url}'>`;
 }
